0b06534a28f96256edd26b90335bec4e075fb15f
Uzia Peer <uzia.Peer@msmail.ariel.ac.il>
2026-08-02 15:44:58 +0300

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -2,16 +2,25 @@ def sorted_subset_sums(numbers: set):
     import heapq
 
     numbers = sorted(numbers)
-
-    heap = [(0, 0)]
+    heap = [(0, -1)]
+    seen = {(0, -1)}
 
     while heap:
         total, i = heapq.heappop(heap)
         yield total
 
-        if i < len(numbers):
-            heapq.heappush(heap, (total + numbers[i], i + 1))
-            heapq.heappush(heap, (total, i + 1))
+        j = i + 1
+        if j < len(numbers):
+            option1 = (total + numbers[j], j)
+            if option1 not in seen:
+                seen.add(option1)
+                heapq.heappush(heap, option1)
+
+            if i >= 0:
+                option2 = (total - numbers[i] + numbers[j], j)
+                if option2 not in seen:
+                    seen.add(option2)
+                    heapq.heappush(heap, option2)
 
 if __name__ == '__main__':
     # from itertools import takewhile, islice
